ee2a4f49339ac539fcaf23db12afd9d43d6e10ec
Luiz Gabriel <gabrielon6689@gmail.com>
2025-07-17 15:05:18 -0300

    Format code

--- 03-API-SOLID/src/env/index.ts
+++ 03-API-SOLID/src/env/index.ts
@@ -1,16 +1,15 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
 })
 
-//Validação 
+// Validação
 const _env = envSchema.safeParse(process.env)
 
-//Verifica a validação
+// Verifica a validação
 if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
 
