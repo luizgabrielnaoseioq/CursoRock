b64775b6218043ff972fbee7d7e298e1d17e6456
Luiz Gabriel <gabrielon6689@gmail.com>
2025-07-18 11:57:47 -0300

    docs: documentation created

--- 03-API-SOLID/src/repositories/prisma-users-repository.ts
+++ 03-API-SOLID/src/repositories/prisma-users-repository.ts
@@ -1,6 +1,24 @@
 import { prisma } from '@/lib/prisma'
 import { Prisma } from 'generated/prisma'
 
+/**
+ * Este arquivo define o repositório de usuários utilizando o Prisma ORM.
+ *
+ * - Importa a instância do Prisma configurada em '@/lib/prisma'.
+ * - Importa os tipos do Prisma gerados automaticamente, em especial o tipo 'UserCreateInput'.
+ *
+ * Classe:
+ *
+ * PrismaUsersRepository:
+ *   - Método 'create(data: Prisma.UserCreateInput)':
+ *     - Recebe os dados necessários para criar um novo usuário no banco de dados.
+ *     - Utiliza o método 'prisma.user.create' para inserir o usuário.
+ *     - Retorna o usuário criado.
+ *
+ * Este repositório serve como uma camada de abstração para operações relacionadas a usuários,
+ * facilitando a manutenção e possíveis trocas de implementação futura.
+ */
+
 export class PrismaUsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
