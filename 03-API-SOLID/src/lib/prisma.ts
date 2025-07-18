9252f62bce16620ca416d6a32229e0ed0d49d61b
Luiz Gabriel <gabrielon6689@gmail.com>
2025-07-18 10:43:45 -0300

    refactor: Creating layers in the application

--- 03-API-SOLID/src/lib/prisma.ts
+++ 03-API-SOLID/src/lib/prisma.ts
@@ -1,3 +1,8 @@
+import { env } from '@/env'
 import { PrismaClient } from 'generated/prisma'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+  // Se o ambiente for de desenvolvimento,
+  // mostra as queries no console
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
